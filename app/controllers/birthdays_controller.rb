5c4912a0ef50cb7285a948695cde0ffc444ed5a0
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-07-27 19:23:56 -0400

    finished reminder and birthdays controllers

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -1,2 +1,50 @@
 class BirthdaysController < ApplicationController
+    before_action :find_birthday, only: [:show, :edit, :update, :destroy]
+
+    def show
+    end
+
+    def index
+        @birthdays = Birthday.all
+    end
+
+    def new
+        @birthday = Birthday.new
+    end
+
+    def create
+        @birthday = Birthday.create(birthday_params)
+        if @birthday.valid?
+            redirect_to birthday_path(@birthday)
+        else
+            flash[:my_errors] = @birthday.errors.full_messages
+            redirect_to new_birthday_path
+        end
+    end
+
+    #might need an extra variable here
+    def edit
+    end
+
+    def update
+        if @birthday.update(birthday_params)
+            redirect_to birthday_path(@birthday)
+        else
+            flash[:my_errors] = @birthday.errors.full_messages
+            redirect_to edit_birthday_path
+        end
+    end
+
+    def destroy
+        @birthday.destroy
+    end
+
+    private
+    def birthday_params
+        params.require(:birthday).permit(:gifteename, :dob, :email, :message, :image_url, :user_id, :reminder_id)
+    end
+
+    def find_birthday
+        @birthday = Birthday.find(params[:id])
+    end
 end
--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,23 +1,37 @@
 class RemindersController < ApplicationController
+    before_action :reminder_find, only: [:edit, :update, :destroy]
 
     def new
         @reminder = Reminder.new
         @frequencies = ["Bi-weekly", "Weekly", "Monthly"]
     end
 
-    
+    #double check these paths
     def create
         @reminder = Reminder.create(reminder_params)
         if @reminder.valid?
             redirect_to birthday_path(@reminder.birthday)
         else
+            flash[:my_errors] = @reminder.errors.full_messages
+            redirect_to new_reminder_path
         end
     end
 
     def edit
+        @frequencies = ["Bi-weekly", "Weekly", "Monthly"]
     end
 
     def update
+        if @reminder.update(reminder_params)
+            redirect_to birthday_path(@reminder.birthday)
+        else
+            flash[:my_errors] = @reminder.errors.full_messages
+            redirect_to edit_reminder_path
+        end
+    end
+
+    def destroy
+        @reminder.destroy
     end
 
     private
